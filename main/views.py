cc6eb6827bc36785b09aa907830dc26db07c8729
joshnyamsi <83524711+joshnyamsi@users.noreply.github.com>
2023-03-02 03:27:15 -0500

    Update views.py

--- main/views.py
+++ main/views.py
@@ -2,11 +2,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def signin(request):
-    return render(request, 'signin.html')
+def login(request):
+    return render(request, 'login.html')
 def homepage(request):
     return render(request, 'homepage.html')
 def signup(request):
     return render(request, 'signup.html')
-def setting(request):
-    return render(request, 'setting.html')
+def profile(request):
+    return render(request, 'profile.html')
