f6760a90143e7c613a54415e11579ed61f1ed0cc
Kyle Willoughby <79543718+Willoughby-Kyle@users.noreply.github.com>
2023-03-13 21:42:46 -0400

    Update urls.py

--- main/urls.py
+++ main/urls.py
@@ -2,8 +2,23 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.login, name='login'),
-    path('signup/', views.signup, name='signup'),
+    path('account/', views.account, name='account'),
+    
+    path('', views.home, name='home'),
+
+    path('newpost/', views.newpost, name='newpost'),
+
+    path('newsfeed/', views.newsfeed, name='newsfeed'),
+
+    path('postdetails/', views.postdetails, name='postdetails'),
+
     path('profile/', views.profile, name='profile'),
-    path('homepage/', views.homepage, name='homepage'),
+
+    path('search/', views.search, name='search'),
+
+    path('settings/', views.settings, name='settings'),
+
+    path('signin/', views.signin, name='signin'),
+
+    path('signup/', views.signup, name='signup')
 ]
