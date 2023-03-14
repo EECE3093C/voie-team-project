3a9d5ac49931cb934be3eb169dcad638faa99226
Kyle Willoughby <79543718+Willoughby-Kyle@users.noreply.github.com>
2023-03-13 21:58:07 -0400

    Update and rename function.js to script.js

--- static/js/function.js
+++
@@ -1,3 +0,0 @@
-function toHomepage(){
-    window.location.href= '/homepage';
-}
---
+++ static/js/script.js
@@ -0,0 +1,3 @@
+/*--    Voie Authors:   Hannah Krzywkowski | Josh Nyamsi | Kyle Willoughby | Quoc Luong     --*/
+/*--    Creation Date:  March 03, 2023                                                      --*/
+/*--    Project Name:   FitConnect                                                          --*/
